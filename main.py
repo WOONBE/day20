30444d39b0f1901da45c705ac65745792f063718
WOONBE <kepull2918@naver.com>
2022-04-09 14:26:45 +0900

    0404

--- main.py
+++ main.py
@@ -35,14 +35,19 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+        # game_is_on = False
+        # scoreboard.game_over()
 
     #Detect collision with tail.
-    for segment in snake.segments[1:]: #slicing for exception of head
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    for segment in snake.segments: #slicing for exception of head
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+             # game_is_on = False
+            # scoreboard.game_over()
 
 
     #if head collides with any segment in the tail:
